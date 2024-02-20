65d8c6ac06b3d9f3167a50c4b350ed9c5eca55b3
Bruno Escada <bruno.escada@criticalsoftware.com>
2024-02-20 16:17:49 +0000

    add source and packages

--- models/staging/client_table.sql
+++ models/staging/client_table.sql
@@ -1,3 +1,14 @@
 with source_client as (
-    Select * from {{source('cliente_table')}}
+    Select * from {{ source('cliente_table', 'clientes') }}
+),
+all_columns as (
+    Select
+        ID_CLIENTE AS ID,
+        NOME_CLIENTE AS CLIENT_NAME,
+        IDADE AS AGE,
+        GENERO AS GENDER,
+        CIDADE AS CITY,
+        CODIGO_POSTAL AS ZIP_CODE
+    FROM source_client
 )
+select * from all_columns
