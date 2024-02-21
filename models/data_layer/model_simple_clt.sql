47023b36a3822086217d6974b31861f501ed930f
Bruno Escada <bruno.escada@criticalsoftware.com>
2024-02-21 12:24:47 +0000

    simulation error

--- models/data_layer/model_simple_clt.sql
+++ models/data_layer/model_simple_clt.sql
@@ -2,7 +2,8 @@ with simple_client as (
     Select
         ID,
         CLIENT_NAME,
-        GENDER 
+        GENDER,
+        TEST_ERROR
     from {{ ref('client_table_staging_en') }}
 ) 
 Select * from simple_client
