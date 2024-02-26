c70199d15d81577ce363d472c70771d1fa187cbf
Bruno Escada <bruno.escada@criticalsoftware.com>
2024-02-26 09:56:20 +0000

    create snapshot client

--- models/staging/client_table_staging_en.sql
+++ models/staging/client_table_staging_en.sql
@@ -8,7 +8,9 @@ all_columns as (
         IDADE AS AGE,
         GENERO AS GENDER,
         CIDADE AS CITY,
-        CODIGO_POSTAL AS ZIP_CODE
+        CODIGO_POSTAL AS ZIP_CODE,
+        Criado_em AS CREATED_AT,
+        Atualizado_em AS UPDATED_AT
     FROM source_client
 )
 select * from all_columns
